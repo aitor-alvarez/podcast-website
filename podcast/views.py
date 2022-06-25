b91f4951d35717acb018891ab888ca90d54afd23
aitor-arronte <aitoraalvarez@gmail.com>
2022-06-24 16:13:10 -1000

    added home

--- podcast/views.py
+++ podcast/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from .models import *
 
 
-def get_podcasts(request, lang=None):
-	podcasts = Podcast.objects.all().order_by('-id')
-	return render(request, template_name='podcast/podcasts.html', context={'podcasts':podcasts})
+
+
 
 
--- podcast_website/urls.py
+++ podcast_website/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from podcast.views import *
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from podcast_website import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='podcast/home.html'), name='home'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
