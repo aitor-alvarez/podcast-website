f1bc5c002d7b926556d8efec3883f9969e158956
aitor-arronte <aitoraalvarez@gmail.com>
2022-10-13 10:48:07 -1000

    added podcast user forms and views

--- podcast_user/views.py
+++ podcast_user/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from forms import SignUpForm
+import random
+import string
 
-# Create your views here.
+
+def request_user(request):
+	letters = string.ascii_lowercase
+	passw = ''.join(random.choice(letters) for i in range(10))
+	if (request.method == 'POST') and request.user.is_anonymous == True:
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			if User.objects.filter(username=form.cleaned_data['email']).exists():
+				return render(request, 'podcast_user/details.html',
+				              {'message': 'The email is currently in use. Please provide another email address.'})
+			else:
+				try:
+					email = form.cleaned_data['email']
+					data = form.save(commit=False)
+					data.username = email
+					data.password = make_password(passw)
+					data.save()
+					return render(request, 'podcast_user/details.html', {'user': email, 'passw': passw})
+
+				except:
+					HttpResponse('The email is currently in use. Please provide another email address.')
+	else:
+		form = SignUpForm()
+	return render(request, 'podcast_user/signup.html', {'form': form})
--- podcast_website/urls.py
+++ podcast_website/urls.py
@@ -2,12 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from podcast.views import *
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from podcast_website import settings
 from podcast.views import autocomplete
+from podcast_user.views import *
+
 
 urlpatterns = [
+    path('accounts/', include(('django.contrib.auth.urls','django.contrib.auth'), namespace='auth'), name="signin"),
+    path('accounts/password_change/',TemplateView.as_view(template_name="registration/password_change_form.html"), name="password_change"),
+    path('password-done', TemplateView.as_view(template_name="registration/password_change_done.html"), name="password_change_done"),
     path('grappelli/', include('grappelli.urls')),
+    path('request-user/', request_user, name='request-user'),
+		path('password_reset/',auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+		path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+		path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+		path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
     path('search/', PodcastSearch.as_view()),
