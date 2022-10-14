02693ed6b350a77f00c24537853cf3dc79d1d23b
aitor-arronte <aitoraalvarez@gmail.com>
2022-10-13 16:04:31 -1000

    annotator

---
+++ audio_annotator/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from podcast.models import Podcast
+
+class PodcastAnnotation(models.Model):
+	podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+	annotations = models.ManyToManyField('Annotation', blank=True, null=True)
+
+	def __str__(self):
+		return self.podcast.title
+
+
+class Annotation(models.Model):
+	annotation = models.TextField()
+
